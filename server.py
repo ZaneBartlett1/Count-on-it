43a1cc226b32307adccafc3b7cfee2240554d20f
Zane Bartlett <zanebartlet1@gmail.com>
2023-01-29 22:41:28 -0600

    wip

---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import Flask, send_from_directory
+import random
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+# Path for our main Svelte page
+@app.route("/")
+def base():
+    return send_from_directory('client/public', 'index.html')
+
+# Path for all the static files (compiled JS/CSS, etc.)
+@app.route("/<path:path>")
+def home(path):
+    return send_from_directory('client/public', path)
+
+
+@app.route("/rand")
+def hello():
+    return str(random.randint(0, 100))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
