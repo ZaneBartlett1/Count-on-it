9e4d5262dcdcd83a7b9c287c3ab682b8589d9d9f
Zane Bartlett <85081861+ZaneBartlett1@users.noreply.github.com>
2023-05-04 18:16:57 -0500

    added reason for not using static paths

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,9 @@
 import pytest
 from server import app
 
+
 @pytest.fixture
 def client():
-    app.config['TESTING'] = True
+    app.config["TESTING"] = True
     with app.test_client() as client:
         yield client
--- tests/test_server.py
+++ tests/test_server.py
@@ -1,9 +1,10 @@
 import pytest
 import json
 
+
 # tests/test_counters.py
 def test_get_counters(client):
-    response = client.get('/counters')
+    response = client.get("/counters")
     assert response.status_code == 200
     counters = json.loads(response.data)
     assert isinstance(counters, list)
