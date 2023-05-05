f77514d3c25af1f4e95e68c10af68273bb7272b6
Zane Bartlett <85081861+ZaneBartlett1@users.noreply.github.com>
2023-05-04 21:32:55 -0500

    Added types and JSDocs for AddCounter.svelte

--- client/src/lib/AddCounter.svelte
+++ client/src/lib/AddCounter.svelte
@@ -1,24 +1,31 @@
-<script>
-  import { createEventDispatcher } from 'svelte';
-  import Counter from './Counter.svelte';
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+  import Counter from "./Counter.svelte";
 
-  let name = '';
+  let name = "";
 
   const dispatch = createEventDispatcher();
 
-  function handleSubmit(event) {
+  /**
+   * Handles the form submission, adding a new counter.
+   * @param {Event} event - The submit event.
+   */
+  function handleSubmit(event: Event) {
     event.preventDefault();
     const newCounter = { name, count: 0 };
-    fetch(`http://127.0.0.1:5000/add-counter/${name}`, {method: 'POST'});
+    fetch(`http://127.0.0.1:5000/add-counter/${name}`, { method: "POST" });
     // Dispatch an event to notify the main component that a new counter was added
-    dispatch('addCounter', { detail: newCounter });
+    dispatch("addCounter", { detail: newCounter });
     // Reset the name
-    name = '';
+    name = "";
   }
-
 </script>
 
 <form on:submit={handleSubmit}>
-  <input type="text" bind:value={name} placeholder="Enter a name for the new counter" />
+  <input
+    type="text"
+    bind:value={name}
+    placeholder="Enter a name for the new counter"
+  />
   <button type="submit">Add counter</button>
 </form>
